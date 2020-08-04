eefe54f061c40799bc213e742655efd90f60c169
Dave Winniford <dwinniford@gmail.com>
2020-08-04 14:23:31 -0400

    adds polly form

--- src/components/ReadForm.js
+++ src/components/ReadForm.js
@@ -12,9 +12,15 @@ export default function ReadForm() {
     }
 
     return (
-        <form onSubmit={event => onReadSubmit(event)}>
-            <p>{text}</p>
-            <input type="submit" />
-        </form>
+        <div>
+            <div id="textToSynth">
+                <input autofocus size="23" type="text" id="textEntry" value="It's very good to meet you."/>
+                <button class="btn default" onClick="speakText()">Synthesize</button>
+                <p id="result">Enter text above then click Synthesize</p>
+            </div>
+            <audio id="audioPlayback" controls>
+                <source id="audioSource" type="audio/mp3" src="" />
+            </audio>
+        </div>
     )
 }
