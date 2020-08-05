942f4e7da4be6741e82b046286979cca4da0efac
Dave Winniford <dwinniford@gmail.com>
2020-08-05 14:04:57 -0400

    uses ref to load audio

--- src/components/ReadForm.js
+++ src/components/ReadForm.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useRef} from 'react'
 import AWS from 'aws-sdk'
 
 
@@ -7,13 +7,8 @@ export default function ReadForm() {
     const [text, setText] = useState('What would you like me to say?')
     const [audioSource, setAudioSource] = useState('')
     const [result, setResult] = useState("Enter text above then click Synthesize")
-    
-    // const onReadSubmit = (event) => {
-    //     event.preventDefault()
-    //     fetch('http://localhost:7777/read')
-    //         .then(res => res.json())
-    //         .then(json => setText(json.message))
-    // }
+
+    const audioRef = useRef(null)
 
     AWS.config.region = process.env.REACT_APP_AWS_REGION; 
     AWS.config.credentials = new AWS.CognitoIdentityCredentials({IdentityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID});
@@ -24,11 +19,10 @@ export default function ReadForm() {
         const speechParams = {
             OutputFormat: "mp3",
             SampleRate: "16000",
-            Text: "",
-            TextType: text,
+            Text: text,
+            TextType: "text",
             VoiceId: "Matthew"
         };
-        speechParams.Text = "Sample Text";
         const polly = new AWS.Polly({apiVersion: '2016-06-10'}) // need {apiVersion: '2016-06-10'} ?
         const signer = new AWS.Polly.Presigner(speechParams, polly)
         signer.getSynthesizeSpeechUrl(speechParams, function(error, url) {
@@ -36,7 +30,9 @@ export default function ReadForm() {
                 setResult(error);
             } else {
                 setAudioSource(url);
-                document.getElementById('audioPlayback').load();
+                // document.getElementById('audioPlayback').load();
+                audioRef.current.load()
+                // is this a good use case for a ref
                 setResult("Speech ready to play.");
             }
         })
@@ -53,12 +49,11 @@ export default function ReadForm() {
             <div id="textToSynth">
                 <input onChange={event => handleInputChange(event)} autoFocus size="23" type="text" id="textEntry" value={text}/>
                 <button className="btn default" onClick={() => speakText()}>Synthesize</button>
-    <p id="result">{result}</p>
+                <p id="result">{result}</p>
             </div>
-            <audio id="audioPlayback" controls>
+            <audio ref={audioRef} id="audioPlayback" controls>
                 <source id="audioSource" type="audio/mp3" src={audioSource} />
             </audio>
-            {/* <script src="https://sdk.amazonaws.com/js/aws-sdk-2.726.0.min.js"></script> */}
         </div>
     )
 }
